e3d94b2d37b7ea407034fa3fe3ded158a8b795bc
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:57:35 +0200

    Test report step has no OAuth refresh token

---
+++ test/meta-issue-report-workflow-report-no-refresh.test.js
@@ -0,0 +1,9 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('dedicated report step contains no OAuth refresh token',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ const report=workflow.split('- name: Run sanitized read-only Meta issue report')[1];
+ assert.doesNotMatch(report,/REFRESH_TOKEN/);
+});
