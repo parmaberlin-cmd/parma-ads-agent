35df21644f1b367292259845d1b54b6efc7806d9
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:55:29 +0200

    Test report step has no arbitrary URL inputs

---
+++ test/meta-issue-report-workflow-report-no-url.test.js
@@ -0,0 +1,9 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('dedicated report step contains no arbitrary URL or destination input',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ const report=workflow.split('- name: Run sanitized read-only Meta issue report')[1];
+ assert.doesNotMatch(report,/https?:\/\/|DESTINATION|URL/);
+});
