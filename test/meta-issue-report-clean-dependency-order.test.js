bfd57d93121ec1a663228649170f0d9de8ecc16a
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 16:07:59 +0200

    Test secrets introduced after dependency installation

---
+++ test/meta-issue-report-clean-dependency-order.test.js
@@ -0,0 +1,8 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('dependencies are installed before Meta secrets are introduced',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ assert.ok(workflow.indexOf('run: npm ci') < workflow.indexOf('META_ACCESS_TOKEN'));
+});
