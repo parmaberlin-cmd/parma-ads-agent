3b79823b420d794fa1b80f36e02aa863f57e8e3a
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:44:39 +0200

    Test diagnostic workflow runs once per dispatch

---
+++ test/meta-issue-report-workflow-no-matrix.test.js
@@ -0,0 +1,8 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('Meta diagnostic workflow has no matrix fan-out',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ assert.doesNotMatch(workflow,/matrix:/);
+});
