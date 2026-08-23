cc9eb0dfc198feb4f7c2f44708942580480c8b36
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:57:01 +0200

    Test report step has no Page token inputs

---
+++ test/meta-issue-report-workflow-report-no-page-token.test.js
@@ -0,0 +1,9 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('dedicated report step contains no separate Page token',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ const report=workflow.split('- name: Run sanitized read-only Meta issue report')[1];
+ assert.doesNotMatch(report,/PAGE_ACCESS_TOKEN/);
+});
