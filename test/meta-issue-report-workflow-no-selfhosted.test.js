abe0d53f75ea4ab0fbbc0cbaff8d6682762ed7da
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:48:30 +0200

    Test diagnostic workflow avoids self-hosted runners

---
+++ test/meta-issue-report-workflow-no-selfhosted.test.js
@@ -0,0 +1,8 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('Meta diagnostic workflow does not use self-hosted runners',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ assert.doesNotMatch(workflow,/self-hosted/);
+});
