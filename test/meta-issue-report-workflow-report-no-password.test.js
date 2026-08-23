a081caabe20e946781da0134abf5e2f6efbc0350
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:57:25 +0200

    Test report step has no password inputs

---
+++ test/meta-issue-report-workflow-report-no-password.test.js
@@ -0,0 +1,9 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('dedicated report step contains no password or username credential',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ const report=workflow.split('- name: Run sanitized read-only Meta issue report')[1];
+ assert.doesNotMatch(report,/PASSWORD|USERNAME/);
+});
