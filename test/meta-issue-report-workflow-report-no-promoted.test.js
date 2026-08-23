4c3800a5d54e2416d7f924ed3421fc4e7bf83770
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:56:45 +0200

    Test report step has no promoted object inputs

---
+++ test/meta-issue-report-workflow-report-no-promoted.test.js
@@ -0,0 +1,9 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('dedicated report step contains no promoted object write input',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ const report=workflow.split('- name: Run sanitized read-only Meta issue report')[1];
+ assert.doesNotMatch(report,/PROMOTED_OBJECT/);
+});
