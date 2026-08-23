b892a0b6a5d54b69f0b13080a1eda7effc421c3c
parmaberlin-cmd <parmaberlin@gmail.com>
2026-08-23 15:56:30 +0200

    Test report step has no lifetime budget inputs

---
+++ test/meta-issue-report-workflow-report-no-lifetime.test.js
@@ -0,0 +1,9 @@
+const test=require('node:test');
+const assert=require('node:assert/strict');
+const fs=require('node:fs');
+
+test('dedicated report step contains no lifetime budget write input',()=>{
+ const workflow=fs.readFileSync('.github/workflows/meta-issue-report.yml','utf8');
+ const report=workflow.split('- name: Run sanitized read-only Meta issue report')[1];
+ assert.doesNotMatch(report,/LIFETIME_BUDGET/);
+});
