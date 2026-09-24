9413fc34fb21f05eb19ed13da23a0c109f6dade8
parmaberlin-cmd <parmaberlin@gmail.com>
2026-09-24 10:36:14 -0700

    Record reported manual Story asset fingerprint

---
+++ instagram-manual-publication-seed.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = Object.freeze([
+  Object.freeze({
+    publication_id: 'manual-story-20260924-book-a-table',
+    account: 'parma.divinibenedetti',
+    asset_sha256: '5a299f13759e1e9a10d887209e5a9c58cfa783c6608c298924e8e6c6f266fb65',
+    content_type: 'STORIES',
+    source_asset: 'parma-story-2026-09-24-book-a-table.mp4',
+    provider_verified: false,
+    reported_at: '2026-09-24',
+  }),
+]);
